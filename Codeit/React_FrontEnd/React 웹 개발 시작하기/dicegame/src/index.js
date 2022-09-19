a29caced304eb67a89ae69fa669dc8bc8d036306
DongHyeok Lim <zx07050@naver.com>
2022-09-19 17:41:07 +0900

    가위바위보 프로젝트 진행 중

--- Codeit/React_FrontEnd/React 웹 개발 시작하기/dicegame/src/index.js
+++ Codeit/React_FrontEnd/React 웹 개발 시작하기/dicegame/src/index.js
@@ -1,11 +1,7 @@
 import ReactDOM from 'react-dom/client';
+import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
-    <h1 id="title">가위바위보</h1>
-    <button className='hand'>가위</button>
-    <button className='hand'>바위</button>
-    <button className='hand'>보</button>
-  </>
-);
+
+root.render(<App />);
